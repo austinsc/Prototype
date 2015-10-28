f722753a0972de243b127ce667ae6b6183cf7bf2
Stephen Austin <stephen.austin@pwnt.co>
2015-10-28 04:10:28 -0400

    moved to standard asp.net

---
+++ Prototype/Controllers/DataController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Routing;
+using Raven.Abstractions.Data;
+using RiotSharp;
+using RiotSharp.TeamEndpoint;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Prototype.Controllers
+{
+    [RoutePrefix("api/data")]
+    public class DataController : ApiController
+    {
+        private static readonly RiotApi _api = RiotApi.GetInstance("50a768a2-4d69-47fc-806c-8fe097212256");
+        // GET: api/data/load
+        [HttpGet]
+        [Route("load")]
+        public async Task<IHttpActionResult> Load()
+        {
+            using (var bulk = Data.Store.BulkInsert(null, new BulkInsertOptions() { OverwriteExisting = true, SkipOverwriteIfUnchanged = true }))
+            {
+                var league = await _api.GetChallengerLeagueAsync(Region.na, Queue.RankedSolo5x5);
+                foreach (var entry in league.Entries)
+                    bulk.Store(entry, entry.PlayerOrTeamId);
+                var allTeams = new List<Team>();
+                //for (var i = 0; i < league.Entries.Count; i += 10)
+                //{
+                //    var teams = await _api.GetTeamsAsync(Region.na, league.Entries.GetRange(i, 10).Select(x => x.PlayerOrTeamId).ToList());
+                //    foreach (var team in teams)
+                //        bulk.Store(team.Value, team.Key);
+                //    allTeams.AddRange(teams.Values);
+                //}
+                return this.Json(new
+                {
+                    league,
+                    teams = allTeams
+                });
+            }
+        }
+    }
+}
---
+++ Prototype/Data.cs
@@ -0,0 +1,53 @@
+﻿﻿// Copyright (c) 2015 PartsSource, Inc. All Right Reserved.
+// 
+// This file contains proprietary trade secret information and is the property of PartsSource, Inc.  Reproduction 
+// or transmission in any form or by any means, electronic, mechanical or otherwise, is prohibited without 
+// express prior written permission.
+// 
+// Filename:    Data.cs
+// Modified On: 04/22/2015 1:56 PM
+// Modified By: stephen.austin (stephen.austin)
+
+using System;
+using Raven.Client;
+using Raven.Client.Document;
+using Raven.Client.FileSystem;
+using Raven.Imports.Newtonsoft.Json;
+using Raven.Imports.Newtonsoft.Json.Converters;
+
+namespace Prototype
+{
+    public static class Data
+    {
+        private static readonly Lazy<IDocumentStore> _documentStore = new Lazy<IDocumentStore>(() =>
+        {
+            var store = new DocumentStore
+            {
+                Url = ConnectionString,
+                DefaultDatabase = DatabaseName
+            }.Initialize();
+            store.Conventions.CustomizeJsonSerializer = serializer =>
+            {
+                serializer.NullValueHandling = NullValueHandling.Ignore;
+                serializer.Converters.Add( new StringEnumConverter() );
+            };
+            return store;
+        });
+
+        private static readonly Lazy<IFilesStore> _fileStore = new Lazy<IFilesStore>(() => new FilesStore
+        {
+            Url = ConnectionString,
+            DefaultFileSystem = DatabaseName
+        }.Initialize());
+
+        public static string ConnectionString { get; } = "http://localhost:8080/";
+
+        public static string DatabaseName { get; } = "no-defeat";
+
+        /// <summary>
+        /// Singleton document store instance used to access the RavenDB server
+        /// </summary>
+        public static IDocumentStore Store => _documentStore.Value;
+        public static IFilesStore Files => _fileStore.Value;
+    }
+}
