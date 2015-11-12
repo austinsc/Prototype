e89cc6f351f69a9005e7adf4a29310a3b40809c1
Stephen Austin <stephen.austin@rovisys.com>
2015-11-12 16:40:31 -0500

    Matches engine working

--- Prototype/Controllers/DataController.cs
+++ Prototype/Controllers/DataController.cs
@@ -5,42 +5,28 @@
 // express prior written permission.
 // 
 // Filename:    DataController.cs
-// Modified On: 10/28/2015 9:29 AM
+// Modified On: 11/12/2015 2:55 PM
 // Modified By: Austin, Stephen (saustin)
 
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Http;
 using Prototype.Scantron;
-using Raven.Abstractions.Data;
-using Raven.Abstractions.Extensions;
-using RiotSharp;
-using RiotSharp.TeamEndpoint;
-
+using RiotSharp.MatchEndpoint;
+using Team = RiotSharp.TeamEndpoint.Team;
 
 namespace Prototype.Controllers
 {
     [RoutePrefix("api/data")]
     public class DataController : ApiController
     {
-        private static readonly BulkInsertOptions _BULK_INSERT_OPTIONS = new BulkInsertOptions
-        {
-            OverwriteExisting = true,
-            SkipOverwriteIfUnchanged = true
-        };
-
         // GET: api/data/load
         [HttpGet]
-        [Route("load")]
-        public async Task<IEnumerable<Team>> Load()
-        {
-            var data = await Engine.RetrieveTeams();
-            using (var bulk = Data.Store.BulkInsert(null, _BULK_INSERT_OPTIONS))
-                data.ForEach(x => bulk.Store(x, $"teams/{x.FullId}"));
-            return data;
-        }
-    }
+        [Route("teams")]
+        public async Task<IEnumerable<Team>> Teams() => await Engine.RetrieveTeams();
 
+        [HttpGet]
+        [Route("matches")]
+        public async Task<IEnumerable<MatchDetail>> Matches() => await Engine.RetrieveMatchDetails();
+    }
 }
--- Prototype/Scantron/Engine.cs
+++ Prototype/Scantron/Engine.cs
@@ -5,32 +5,81 @@
 // express prior written permission.
 // 
 // Filename:    Engine.cs
-// Modified On: 10/28/2015 9:56 AM
+// Modified On: 11/12/2015 4:40 PM
 // Modified By: Austin, Stephen (saustin)
 
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Driver;
+using Newtonsoft.Json;
 using RiotSharp;
-using RiotSharp.TeamEndpoint;
+using RiotSharp.MatchEndpoint;
+using Team = RiotSharp.TeamEndpoint.Team;
 
 namespace Prototype.Scantron
 {
+    public static class Data
+    {
+        private static readonly Lazy<MongoClient> _client = new Lazy<MongoClient>(() => new MongoClient("mongodb://ndit.pwnt.co"));
+        private static readonly Lazy<IMongoDatabase> _database = new Lazy<IMongoDatabase>(() => _client.Value.GetDatabase("no-defeat-in-team"));
+        private static readonly Lazy<IMongoCollection<BsonDocument>> _teams = new Lazy<IMongoCollection<BsonDocument>>(() => _database.Value.GetCollection<BsonDocument>("teams"));
+        private static readonly Lazy<IMongoCollection<BsonDocument>> _matches = new Lazy<IMongoCollection<BsonDocument>>(() => _database.Value.GetCollection<BsonDocument>("matches"));
+
+        public static IMongoCollection<BsonDocument> Teams => _teams.Value;
+        public static IMongoCollection<BsonDocument> Matches => _matches.Value;
+    }
+
     public static class Engine
     {
         private static readonly RiotApi _api = RiotApi.GetInstance("50a768a2-4d69-47fc-806c-8fe097212256");
 
+        static Engine()
+        {
+            //BsonClassMap.RegisterClassMap<Team>(cm =>
+            //{
+            //    cm.AutoMap();
+            //    cm.MapIdMember(c => c.FullId).SetIdGenerator(StringObjectIdGenerator.Instance);
+            //});
+            ConventionRegistry.Register("camel case", new ConventionPack { new CamelCaseElementNameConvention() }, t => true);
+        }
+
         public static async Task<IEnumerable<Team>> RetrieveTeams()
         {
-            var allTeams = new List<Team>();
-            var league = await _api.GetChallengerLeagueAsync(Region.na, Queue.RankedSolo5x5);
+            var result = new List<Team>();
+            var league = await _api.GetChallengerLeagueAsync(Region.na, Queue.RankedTeam5x5);
             foreach(var chunk in league.Entries.Chunk(10))
             {
-                var teams = await _api.GetTeamsAsync(Region.na, chunk.Select(x => int.Parse(x.PlayerOrTeamId)).ToList());
-                allTeams.AddRange(teams.SelectMany(x => x.Value));
+                var teams = await _api.GetTeamsAsync(Region.na, chunk.Select(x => x.PlayerOrTeamId).ToList());
+                result.AddRange(teams.Values);
             }
-            return allTeams;
+            await Data.Teams.InsertManyAsync(result.Select(team => BsonDocument.Parse(JsonConvert.SerializeObject(team))));
+            return result;
+        }
+
+        public static async Task<IEnumerable<MatchDetail>> RetrieveMatchDetails(Region region = Region.na)
+        {
+            var challenger = await _api.GetChallengerLeagueAsync(region, Queue.RankedTeam5x5);
+            var masters = await _api.GetMasterLeagueAsync(region, Queue.RankedTeam5x5);
+            var result = new List<MatchDetail>();
+            foreach(var chunk in challenger.Entries.Concat(masters.Entries).Chunk(10))
+            {
+                var teams = await _api.GetTeamsAsync(region, chunk.Select(x => x.PlayerOrTeamId).ToList());
+                foreach(var id in teams.Values.SelectMany(x => x.MatchHistory).Select(x => x.GameId).Distinct())
+                    if(await Data.Matches.CountAsync(Builders<BsonDocument>.Filter.Eq("matchId", id), new CountOptions { Limit = 1 }) == 0)
+                    {
+                        var match = await _api.GetMatchAsync(region, id, true);
+                        if(match != null)
+                        {
+                            await Data.Matches.InsertOneAsync(BsonDocument.Parse(JsonConvert.SerializeObject(match)));
+                            result.Add(match);
+                        }
+                    }
+            }
+            return result;
         }
     }
 }
